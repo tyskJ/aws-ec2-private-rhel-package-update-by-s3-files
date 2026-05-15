506a1ad3d248a4b86e6e4168b86283d430b6d8ac
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-05-15 08:36:51 +0000

    :recycle: Subnet定義汎用化

--- modules/subnet/locals.tf
+++ modules/subnet/locals.tf
@@ -2,14 +2,23 @@ locals {
   subnets = {
     private_ec2_1a = {
       name       = "private-subnet-ec2-1a"
-      az         = "a"
-      cidr       = "10.0.1.0/24"
+      cidr_bits  = 8
+      cidr_idnex = 1
+      az_index   = 0
       map_public = false
     }
-    private_eni_1a = {
-      name       = "private-subnet-eni-1a"
-      az         = "a"
-      cidr       = "10.0.2.0/24"
+    private_endpoint_1a = {
+      name       = "private-subnet-endpoint-1a"
+      cidr_bits  = 8
+      cidr_idnex = 2
+      az_index   = 0
+      map_public = false
+    }
+    private_mountpoint_1a = {
+      name       = "private-subnet-mountpoint-1a"
+      cidr_bits  = 8
+      cidr_idnex = 3
+      az_index   = 0
       map_public = false
     }
   }
--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -5,8 +5,8 @@ resource "aws_subnet" "this" {
   for_each = local.subnets
 
   vpc_id                  = var.vpc_id
-  availability_zone       = "${var.region}${each.value.az}"
-  cidr_block              = each.value.cidr
+  cidr_block              = cidrsubnet(var.vpc_cidr, each.value.cidr_bits, each.value.cidr_idnex)
+  availability_zone       = data.aws_availability_zones.azs.names[each.value.az_index]
   map_public_ip_on_launch = each.value.map_public
   tags = {
     Name = each.value.name
