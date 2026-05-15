bebbdb326499dd7ff0cd89b4e4e9bd47e232362e
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-05-15 08:35:13 +0000

    :recycle: CIDR変数化

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,7 +2,7 @@
 VPC
 ************************************************************/
 resource "aws_vpc" "this" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
