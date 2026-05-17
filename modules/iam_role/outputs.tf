f901f5d35cc994bf19bf1e10390587fe330d0107
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-05-17 14:26:50 +0900

    :recycle: S3 Files 用IAM追加

--- modules/iam_role/outputs.tf
+++ modules/iam_role/outputs.tf
@@ -5,3 +5,7 @@ output "name_private_ec2_instance_profile" {
 output "name_public_ec2_instance_profile" {
   value = aws_iam_instance_profile.public_ec2.name
 }
+
+output "arn_s3_files_role" {
+  value = aws_iam_role.s3_files.arn
+}
